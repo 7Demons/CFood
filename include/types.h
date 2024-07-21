c1e5d3c8c31abc19566444bc758081581229e890
crero706 <crero706@student.otago.ac.nz>
2024-07-21 17:54:32 +1200

    feat/get-console-size

--- include/types.h
+++ include/types.h
@@ -12,19 +12,18 @@ typedef char C8;
 typedef wchar_t C16;
 
 // Signed integer types
-typedef int16_t S16; // -32,768 to 32,767
-typedef int32_t S32; // -2,147,483,648 to 2,147,483,647
-typedef int64_t S64;// -9,223,372,036,854,775,808 to 9,223,372,036,85
+typedef int16_t I16; // -32,768 to 32,767
+typedef int32_t I32; // -2,147,483,648 to 2,147,483,647
+typedef int64_t I64; // -9,223,372,036,854,775,808 to 9,223,372,036,85
 
 // Unsigned integer types
 typedef void U0;
 typedef uint16_t U16; // 0 to 65,535
 typedef uint32_t U32; // 0 to 4,294,967,295
-typedef uint64_t U64;// 0 to 18,446,744,073,709,551,615
+typedef uint64_t U64; // 0 to 18,446,744,073,709,551,615
 
 // Floats
-typedef float F32; // 3.4E +/- 38 (seven digits)
+typedef float F32;  // 3.4E +/- 38 (seven digits)
 typedef double F64; // 1.7E +/- 308 (fifteen digits)
 
 #endif // __TYPES_H__
-
--- src/main.c
+++ src/main.c
@@ -1,51 +1,54 @@
-#include <stdio.h>
 #include "../include/file.h"
+#include "../include/types.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+#define SLEEP_TIME_MS 40
 // TODO: configure CMAKE with this project or something.
 // if you are to add another .c file add it to tasks.json for now
+
+typedef struct consoleSize_s {
+    I32 rows;
+    I32 cols;
+} ConsoleSize;
+
+void updateConsoleSize(ConsoleSize *consoleSize) {
+    HANDLE hConsole;
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    if (GetConsoleScreenBufferInfo(hConsole, &consoleInfo)) {
+        consoleSize->cols = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+        consoleSize->rows = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+    }
+    // unable to retrieve console size, we will assume it hasnt changed.
+}
 
 int main() {
     // start up routines
     // load recipes
     DirectoryTable table = createDirectoryTable();
 
-    LoadDirectoryContents(&table, "C:\\Users\\Admin\\source\\repos\\CFood\\data");
+    LoadDirectoryContents(&table, "C:\\Users\\Robert\\source\\repos\\CFood\\data");
     printDirectoryTable(&table);
 
-        // set up states
+    // set up states
+    ConsoleSize consoleSize;
 
-    // main loop
-        // input handler
-        // update logic
-        // output rendering
-        // exit?
+    while (true) {
+        system("cls"); // this will cause flickering. look into SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+        updateConsoleSize(&consoleSize);
+        printf("Size: cols: %d rows: %d\n", consoleSize.cols, consoleSize.rows);
+
+        Sleep(SLEEP_TIME_MS);
+    }
+    // input handler
+    // update logic
+    // output rendering
+    // exit?
 
     // clean up
     return 0;
-
 }
-
-/*
-S32 main() {
-    HANDLE hConsole;
-    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-    int columns, rows;
-
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    if (GetConsoleScreenBufferInfo(hConsole, &consoleInfo)) {
-        columns = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
-        rows = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
-
-        printf("Console size: %d columns, %d rows\n", columns, rows);
-    } else {
-        printf("Unable to retrieve console size.\n");
-    }
-
-
-    DirectoryTable table = createDirectoryTable();
-
-    LoadDirectoryContents(&table, "C:\\Users\\Admin\\source\\repos\\CFood\\data");
-    printDirectoryTable(&table);
-}
-*/
