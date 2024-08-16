9dd1891a1a555d7ba0d2be1c285fa38d9392505f
Jenny Puzey <puzje251>
2024-08-16 15:59:52 +1200

    setting up cmake

---
+++ build/CMakeFiles/cfood.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cfood.dir/main.c.o"
+  "CMakeFiles/cfood.dir/main.c.o.d"
+  "cfood"
+  "cfood.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cfood.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cfood.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cfood.
+# This may be replaced when dependencies are built.
